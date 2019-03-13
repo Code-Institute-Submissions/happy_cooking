6a49265577e5a84d59e596ab73ac33daeedacca5
Noelle Browne <noelle.browne@yahoo.ie>
2019-03-13 14:37:07 +0000

    added login route and added secret keys and mongo URI key

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, escape, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["MONGO_DBNAME"] = 'happy_cooking'
-app.config["MONGO_URI"] = 'mongodb+srv://nellie10:Anything10@cluster0-ryuq4.mongodb.net/happy_cooking?retryWrites=true'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
@@ -18,15 +20,20 @@ def index():
     
 @app.route('/recipes')
 def recipes():
-    return render_template("recipes.html",
-                            recipes=mongo.db.recipes.find())
+    recipes = mongo.db.recipes.find({})
+    return render_template("recipes.html", recipes=recipes)
+
 
 
 @app.route('/my_recipes')
 def my_recipes():
-    return render_template("my_recipes.html",
-     
-                            my_recipes=mongo.db.my_recipes.find())
+    if session:
+        return render_template("my_recipes.html", 
+            my_recipes=mongo.db.my_recipes.find())
+    else:
+        return ('Login first!')               
+
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe ():
@@ -42,14 +49,37 @@ def edit_recipe(recipe_id):
                            my_recipes=all_my_recipes)
 
     
-
-@app.route('/login')
+    
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template("login.html")
+    username = request.form['username']
+    password = request.form['password']
+    user = mongo.db.users.find({
+        'username': username
+    })
+    user = user[0]
+    if user:
+        if user['password'] == password:
+            session['username'] = username
+            return redirect(url_for('my_recipes'))
+        else:
+            return ('Invalid login credentials')
+    else:
+        return ('Invalid login credentials')
+    return render_template("index")
+
+
 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("add_recipe.html")
+    
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+        
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
